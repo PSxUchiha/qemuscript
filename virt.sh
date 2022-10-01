60e134994c3b04fbd240d4d66d0054a46820f774
PSxUchiha <72612676+PSxUchiha@users.noreply.github.com>
2022-10-02 02:09:12 +0530

    Add files via upload

---
+++ virt.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+yay -S --needed qemu qt-virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat libguestfs
+sudo systemctl start libvirtd.service
+sudo systemctl enable libvirtd.service
+sudo sed -i '$a unix_sock_group = \"libvirt\"' /etc/libvirt/libvirtd.conf
+sudo sed -i '$a unix_sock_ro_perms = \"0777\"' /etc/libvirt/libvirtd.conf
+sudo sed -i '$a unix_sock_rw_perms = \"0770\"' /etc/libvirt/libvirtd.conf
+sudo usermod -aG libvirt priyanshu
+sudo systemctl restart libvirtd.service
