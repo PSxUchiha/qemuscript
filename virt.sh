681d3edaa9da652720f7ab639a04615844fb1f20
PSxUchiha <72612676+PSxUchiha@users.noreply.github.com>
2023-03-11 12:57:56 +0530

    minor improvement

--- virt.sh
+++ virt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S --needed qemu qt-virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat libguestfs
+yay -S --needed qemu virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat libguestfs
 sudo systemctl start libvirtd.service
 sudo systemctl enable libvirtd.service
 sudo sed -i '$a unix_sock_group = \"libvirt\"' /etc/libvirt/libvirtd.conf
@@ -8,3 +8,4 @@ sudo sed -i '$a unix_sock_ro_perms = \"0777\"' /etc/libvirt/libvirtd.conf
 sudo sed -i '$a unix_sock_rw_perms = \"0770\"' /etc/libvirt/libvirtd.conf
 sudo usermod -aG libvirt priyanshu
 sudo systemctl restart libvirtd.service
+sudo virsh net-start default
